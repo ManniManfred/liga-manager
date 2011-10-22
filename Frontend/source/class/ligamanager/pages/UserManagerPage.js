3898c7567baf48dacfc4077d64693bad6a73bbf3
rosskamp17@gmail.com <rosskamp17@gmail.com@74091181-6bcb-f78a-6e66-07c6294ab3cc>
2011-10-22 16:42:43 +0000

    Benutzerverwaltung: Tabelle mit Benutzern

--- Frontend/source/class/ligamanager/pages/UserManagerPage.js
+++ Frontend/source/class/ligamanager/pages/UserManagerPage.js
@@ -14,7 +14,10 @@ qx.Class.define("ligamanager.pages.UserManagerPage",
 	construct: function() {
 		this.base(arguments, new qx.ui.layout.Canvas());
 		
-		
+		this.__userRpc = new qx.io.remote.Rpc(ligamanager.Core.RPC_BACKEND , "ligamanager.Usermanager");
+
+		this.__createUserTable();
+		this.__updateUserTable();
 	},
 
 	/*
@@ -43,5 +46,28 @@ qx.Class.define("ligamanager.pages.UserManagerPage",
 
 	members:
 	{
+		__createUserTable : function() {
+		},
+		
+		__updateUserTable : function() {
+			var Users = this.__userRpc.callSync("GetUsers");
+			
+			var tableModel = new qx.ui.table.model.Simple();
+			
+			tableModel.setColumns( ["username", "firstname", "lastname", "email"] );
+			tableModel.setColumnNamesById( {"username":"Benutzer","firstname":"Vorname", "lastname":"Nachname", "email":"E-Mail"} );
+			tableModel.setDataAsMapArray( Users );
+			
+			var table = new qx.ui.table.Table(tableModel);
+			
+			table.setColumnWidth( 0, 200 );
+			table.setColumnWidth( 1, 200 );
+			table.setColumnWidth( 2, 200 );
+			table.setColumnWidth( 3, 200 );
+			
+			table.setShowCellFocusIndicator( false );
+			this.add( table, {left:20, top:20} );
+		}
+			
 	}
 });
