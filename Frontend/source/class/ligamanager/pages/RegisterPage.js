cd8945e8fd26a620038b12f2c2218485f8bce447
rosskamp17@gmail.com <rosskamp17@gmail.com@74091181-6bcb-f78a-6e66-07c6294ab3cc>
2011-10-23 10:47:18 +0000

    UserManager: Buttons eingebaut (ohne Funktion..)

--- Frontend/source/class/ligamanager/pages/RegisterPage.js
+++ Frontend/source/class/ligamanager/pages/RegisterPage.js
@@ -53,8 +53,10 @@ qx.Class.define("ligamanager.pages.RegisterPage",
 		__userRpc : null,
 		
 		__userField : null,
-		__passwordField : null,
+		__firstNameField : null,
+		__surNameField : null,
 		__emailField : null,
+		__passwordField : null,
 		__statusAtom : null,
 		__appName : null,
 		__form : null,
@@ -97,7 +99,6 @@ qx.Class.define("ligamanager.pages.RegisterPage",
 			// name label
 			var nameLabel = new qx.ui.basic.Label(this.tr("User name") + requireSuffix);
 			contentPane.add(nameLabel, { row: 1, column: 0 });
-
 			// name textbox
 			this.__userField = new qx.ui.form.TextField();
 			this.__userField.setTabIndex(tabIndex++);
@@ -105,44 +106,32 @@ qx.Class.define("ligamanager.pages.RegisterPage",
 			this.__userField.setRequiredInvalidMessage(this.tr("The field %1 is required.", this.tr("Name")));
 			contentPane.add(this.__userField, { row: 1, column: 1});
 			form.add(this.__userField, "", null, "UserName");
+					
+			// first name  label
+			var firstNameLabel = new qx.ui.basic.Label(this.tr("first name") + requireSuffix);
+			contentPane.add(firstNameLabel, { row: 2, column: 0 });
+			// first name field
+			this.__firstNameField = new qx.ui.form.TextField();
+			this.__firstNameField.setTabIndex(tabIndex++);
+			this.__firstNameField.setRequired(true);
+			this.__firstNameField.setRequiredInvalidMessage(this.tr("The field %1 is required.", this.tr("first name")));
+			contentPane.add(this.__firstNameField, { row: 2, column: 1 });
+			form.add(this.__firstNameField, "", null, "FirstName");
+
+			// surname  label
+			var surnameLabel = new qx.ui.basic.Label(this.tr("surname") + requireSuffix);
+			contentPane.add(surnameLabel, { row: 3, column: 0 });
+			// surname field
+			this.__surnameField = new qx.ui.form.TextField();
+			this.__surnameField.setTabIndex(tabIndex++);
+			this.__surnameField.setRequired(true);
+			this.__surnameField.setRequiredInvalidMessage(this.tr("The field %1 is required.", this.tr("surname")));
+			contentPane.add(this.__surnameField, { row: 3, column: 1 });
+			form.add(this.__surnameField, "", null, "LastName");
 			
-			// password label
-			var passwordLabel = new qx.ui.basic.Label(this.tr("Password") + requireSuffix);
-			contentPane.add(passwordLabel,{row: 2, column: 0 });
-
-			// password textbox
-			var passwordField = this.__passwordField = new qx.ui.form.PasswordField();
-			passwordField.setTabIndex(tabIndex++);
-			passwordField.setRequired(true);
-			passwordField.setRequiredInvalidMessage(this.tr("The field %1 is required.", this.tr("Password")));
-			contentPane.add(passwordField,{row: 2, column: 1 });
-			form.add(passwordField, "", function(value) {
-				if (value == null || value.length < 6) {
-					throw new qx.core.ValidationError("", qx.locale.Manager.tr("The password must have a length of 6."));
-				}
-			}, "Password");
-		
-			// confirm password label
-			var confirmPasswordLabel = new qx.ui.basic.Label(this.tr("Confirm password") + requireSuffix);
-			contentPane.add(confirmPasswordLabel,{row: 3, column: 0 });
-
-			// confirm password textbox
-			this.__confirmPasswordField = new qx.ui.form.PasswordField();
-			this.__confirmPasswordField.setTabIndex(tabIndex++);
-			contentPane.add(this.__confirmPasswordField,{row: 3, column: 1 });
-			form.add(this.__confirmPasswordField, "", function(value) {
-				if (value == null || value == "") {
-					throw new qx.core.ValidationError("", qx.locale.Manager.tr("The field %1 is required.", 
-						qx.locale.Manager.tr("Confirm password")));
-				} else if (value != passwordField.getValue()) {
-					throw new qx.core.ValidationError("", qx.locale.Manager.tr("Password and confirm password are not equal."));
-				}
-			}, "ConfirmPassword");
-		
 			// email label
 			var emailLabel = new qx.ui.basic.Label(this.tr("E-Mail") + requireSuffix);
 			contentPane.add(emailLabel, { row: 4, column: 0 });
-
 			// email field
 			this.__emailField = new qx.ui.form.TextField();
 			this.__emailField.setTabIndex(tabIndex++);
@@ -155,24 +144,53 @@ qx.Class.define("ligamanager.pages.RegisterPage",
 			// team label
 			var nameLabel = new qx.ui.basic.Label(this.tr("Team") + suffix);
 			contentPane.add(nameLabel, { row: 5, column: 0 });
-
 			// team textbox
 			this.__teamField = new qx.ui.form.TextField();
 			this.__teamField.setTabIndex(tabIndex++);
 			contentPane.add(this.__teamField, { row: 5, column: 1});
 			//form.add(this.__teamField, "", null, "Team");
 			
-			
+			// password label
+			var passwordLabel = new qx.ui.basic.Label(this.tr("Password") + requireSuffix);
+			contentPane.add(passwordLabel,{row: 6, column: 0 });
+			// password textbox
+			var passwordField = this.__passwordField = new qx.ui.form.PasswordField();
+			passwordField.setTabIndex(tabIndex++);
+			passwordField.setRequired(true);
+			passwordField.setRequiredInvalidMessage(this.tr("The field %1 is required.", this.tr("Password")));
+			contentPane.add(passwordField,{row: 6, column: 1 });
+			form.add(passwordField, "", function(value) {
+				if (value == null || value.length < 6) {
+					throw new qx.core.ValidationError("", qx.locale.Manager.tr("The password must have a length of 6."));
+				}
+			}, "Password");
+		
+			// confirm password label
+			var confirmPasswordLabel = new qx.ui.basic.Label(this.tr("Confirm password") + requireSuffix);
+			contentPane.add(confirmPasswordLabel,{row: 7, column: 0 });
+			// confirm password textbox
+			this.__confirmPasswordField = new qx.ui.form.PasswordField();
+			this.__confirmPasswordField.setTabIndex(tabIndex++);
+			contentPane.add(this.__confirmPasswordField,{row: 7, column: 1 });
+			form.add(this.__confirmPasswordField, "", function(value) {
+				if (value == null || value == "") {
+					throw new qx.core.ValidationError("", qx.locale.Manager.tr("The field %1 is required.", 
+						qx.locale.Manager.tr("Confirm password")));
+				} else if (value != passwordField.getValue()) {
+					throw new qx.core.ValidationError("", qx.locale.Manager.tr("Password and confirm password are not equal."));
+				}
+			}, "ConfirmPassword");
+
 			
 			var btSend = new qx.ui.form.Button(this.tr("Send"));
 			btSend.addListener("execute", this.__register, this);
-			this.add(btSend, {left : 20, top : 210 });
+			this.add(btSend, {left : 20, top : 270 });
 			form.addButton(btSend);
 			
 			
 			// add a reset button
 			var resetButton = new qx.ui.form.Button(this.tr("Reset"));
-			this.add(resetButton, {left: 85, top: 210});
+			this.add(resetButton, {left: 85, top: 270});
 			resetButton.addListener("execute", function() {
 				form.reset();
 			});
--- Frontend/source/class/ligamanager/pages/UserManagerPage.js
+++ Frontend/source/class/ligamanager/pages/UserManagerPage.js
@@ -1,4 +1,9 @@
-
+/*
+#asset(qx/icon/${qx.icontheme}/22/actions/view-refresh.png)
+#asset(qx/icon/${qx.icontheme}/22/actions/document-properties.png)
+#asset(qx/icon/${qx.icontheme}/22/actions/list-add.png)
+#asset(qx/icon/${qx.icontheme}/22/actions/list-remove.png)
+*/
 
 qx.Class.define("ligamanager.pages.UserManagerPage",
 {
@@ -12,11 +17,12 @@ qx.Class.define("ligamanager.pages.UserManagerPage",
 	 */
 
 	construct: function() {
-		this.base(arguments, new qx.ui.layout.Canvas());
+		this.base(arguments, new qx.ui.layout.Canvas);
 		
 		this.__userRpc = new qx.io.remote.Rpc(ligamanager.Core.RPC_BACKEND , "ligamanager.Usermanager");
 
-		this.__createUserTable();
+		this.__createUI();
+		
 		this.__updateUserTable();
 	},
 
@@ -46,27 +52,146 @@ qx.Class.define("ligamanager.pages.UserManagerPage",
 
 	members:
 	{
-		__createUserTable : function() {
+		__userTable : null,
+		__userModel : null,
+		
+		__btnNew : null,
+		__btnDelete : null,
+		__btnEdit : null,
+		
+		
+		//
+		// Event-Handler
+		//
+		
+		__onKeyPressed : function(e) {
+			if (e.getKeyIdentifier() == "Enter") {
+				this.__showUserDialog();
+			}
 		},
 		
+		__onCellClick : function(e) {
+			__btnDelete.setEnabled(true);
+			__btnEdit.setEnabled(true);
+		},
+		
+		__onDblClick : function() {
+			this.__showUserDialog();
+		},
+		
+		__onRefresh : function() {
+			this.__updateUserTable();
+		},
+		
+		__onEdit : function() {
+			this.__showUserDialog();
+		},
+
+		__onDelete : function() {
+			this.__showUserDialog();
+		},
+
+		__onNew : function() {
+			this.__showUserDialog();
+		},
+		
+		//
+		// Data
+		//
+		
+		
+		// show User-Edit Dialog
+		__showUserDialog : function() {
+		},
+		
+		
+		__createUI : function() {
+		
+			var laCaption = new qx.ui.basic.Label(this.tr("User Manager"));
+			laCaption.setFont("bold");
+			laCaption.setPadding(5);
+			laCaption.setBackgroundColor("#CCCCCC");
+			laCaption.setAllowGrowX(true);
+			this.add(laCaption, {left:20, top:20, right:20} );
+	
+	
+			var container = new qx.ui.container.Composite( new qx.ui.layout.Dock() );
+			
+			// 
+			// Toolbar
+			//
+			
+			var bar = new qx.ui.toolbar.ToolBar();
+			var button, part;
+			
+			// refresh
+			part = new qx.ui.toolbar.Part();
+			bar.add(part);
+			
+			button = new qx.ui.toolbar.Button(this.tr("Refresh"), "icon/22/actions/view-refresh.png" );
+			button.addListener("execute", this.__onRefresh, this );
+			part.add(button);
+			
+			
+			// edit, delete, new
+			part = new qx.ui.toolbar.Part();
+			bar.add(part);
+			
+			__btnNew = new qx.ui.toolbar.Button(this.tr("New"), "icon/22/actions/list-add.png" );
+			__btnNew.addListener("execute", this.__onNew, this );
+			part.add(__btnNew);
+
+			__btnDelete = new qx.ui.toolbar.Button(this.tr("Delete"), "icon/22/actions/list-remove.png" );
+			__btnDelete.addListener("execute", this.__onDelete, this );
+			__btnDelete.setEnabled( false );
+			part.add(__btnDelete);
+			
+			__btnEdit = new qx.ui.toolbar.Button(this.tr("Edit"), "icon/22/actions/document-properties.png" );
+			__btnEdit.addListener("execute", this.__onEdit, this );
+			__btnEdit.setEnabled( false );
+			part.add(__btnEdit);
+
+			// tbd.
+			part = new qx.ui.toolbar.Part();
+			bar.add(part);
+			
+			
+			//this.add( bar, {left:20, top:20} );
+			container.add( bar, {edge:"north"} );
+
+			
+			__userTableModel = new qx.ui.table.model.Simple();
+			
+			__userTableModel.setColumns( ["username", "firstname", "lastname", "email"] );
+			__userTableModel.setColumnNamesById( {"username":"Benutzer","firstname":"Vorname", "lastname":"Nachname", "email":"E-Mail"} );
+
+			__userTable = new qx.ui.table.Table(__userTableModel);
+			
+			__userTable.setColumnWidth( 0, 200 );
+			__userTable.setColumnWidth( 1, 200 );
+			__userTable.setColumnWidth( 2, 200 );
+			__userTable.setColumnWidth( 3, 200 );
+			
+			__userTable.setShowCellFocusIndicator( false );
+			
+			__userTable.addListener( "cellClick", this.__onCellClick, this );
+			__userTable.addListener( "dblclick", this.__onDblClick, this );
+			__userTable.addListener( "keypress", this.__onKeyPressed, this );
+			
+			
+			container.add( __userTable, {edge:"center"} );
+			
+			this.add( container, {left:20, top:70, bottom:20 } );
+	
+		},
+		
+		
+		// update data in usertable
 		__updateUserTable : function() {
+		
 			var Users = this.__userRpc.callSync("GetUsers");
 			
-			var tableModel = new qx.ui.table.model.Simple();
-			
-			tableModel.setColumns( ["username", "firstname", "lastname", "email"] );
-			tableModel.setColumnNamesById( {"username":"Benutzer","firstname":"Vorname", "lastname":"Nachname", "email":"E-Mail"} );
-			tableModel.setDataAsMapArray( Users );
-			
-			var table = new qx.ui.table.Table(tableModel);
-			
-			table.setColumnWidth( 0, 200 );
-			table.setColumnWidth( 1, 200 );
-			table.setColumnWidth( 2, 200 );
-			table.setColumnWidth( 3, 200 );
-			
-			table.setShowCellFocusIndicator( false );
-			this.add( table, {left:20, top:20} );
+			__userTableModel.setDataAsMapArray( Users );
 		}
 			
 	}
